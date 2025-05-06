1a3d7d97f1601d9261243f6c89e74543242ef69c
leisquid <leisquid@outlook.com>
2025-05-07 01:20:27 +0800

    init

---
+++ src/leisdefinitions.h
@@ -0,0 +1,27 @@
+/**
+ *  @file   leisdefinitions.h
+ *  @brief  general definitions for Leissoft open-sourced projects.
+ *  Copyright (C) 2023-25 Leisquid Tianyi Li.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#if ! defined __LEISDEFINITIONS_H_
+#   define __LEISDEFINITIONS_H_
+
+#       if ! defined EXIT_SUCCESS
+#           define EXIT_SUCCESS 0
+#       endif /* EXIT_SUCCESS */
+
+#endif /* __LEISDEFINITIONS_H_ */
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/**
+ *  @file   main.cpp
+ *  @brief  the main entry for this ESP32 program.
+ *  Copyright (C) 2023-25 Leisquid Tianyi Li.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#include <Arduino.h>
+
+void setup() {
+    printf("Fucking Slaves get your ass back here!\n");
+}
+
+void loop() {
+}
