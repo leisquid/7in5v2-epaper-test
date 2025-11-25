2dc116d5aeb4233aefdeb35496b2b338fc61f4aa
leisquid <leisquid@outlook.com>
2025-11-25 21:55:44 +0800

    mistake

--- src/test.c
+++ src/test.c
@@ -1,3 +1,22 @@
+/**
+ *  @file   test.c
+ *  @brief  a 4-gray picture array.
+ *  Copyright (C) 2023-25 Leisquid Tianyi Li.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const unsigned char gImage_test[96000] = { /* 0X00,0X02,0X20,0X03,0XE0,0X01, */
 0X55,0X55,0X41,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X55,
 0X55,0X55,0X59,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X55,0X56,0X55,0X95,0X55,0X59,
